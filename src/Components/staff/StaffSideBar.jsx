caba6b69b26f52341131058145dd684509b2941f
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2024-06-10 00:59:09 +0530

    frontUpdate

--- src/Components/staff/StaffSideBar.jsx
+++ src/Components/staff/StaffSideBar.jsx
@@ -1,163 +1,120 @@
-import { React, useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
-import { AiOutlineDashboard } from "react-icons/ai";
-import { FaCartArrowDown } from "react-icons/fa";
-import { FaPerson } from "react-icons/fa6";
-import { IoMdChatboxes } from "react-icons/io";
-import { MdLogout } from "react-icons/md";
-import { MdTour } from "react-icons/md";
-import { MdOutlinePayment } from "react-icons/md";
-import { MdFeedback } from "react-icons/md";
-import { FaHotel } from "react-icons/fa6";
+import React, { useState, useEffect } from 'react';
+import Logout from '../../Assets/icons/logout.png';
+import Profile from '../../Assets/icons/profile.png';
+import { useNavigate } from 'react-router-dom';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import ModeOfTravelIcon from '@mui/icons-material/ModeOfTravel';
+import PaidIcon from '@mui/icons-material/Paid';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import RateReviewIcon from '@mui/icons-material/RateReview';
+import { jwtDecode } from 'jwt-decode';
 
-const StaffSideBar = () => {
-  const [isExpanded, setIsExpanded] = useState(true);
+const StaffSideBar = ({ activeItem }) => {
+  const [activeLink, setActiveLink] = useState(localStorage.getItem('activeLink') || activeItem);
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState(null);
 
-  // Function to toggle the sidebar state
-  const toggleSidebar = () => {
-    setIsExpanded(!isExpanded);
+  const handleLinkClick = (link) => (event) => {
+    event.preventDefault(); // Prevent default navigation behavior
+    setActiveLink(link); // Update active link
+    localStorage.setItem('activeLink', link);
+    if (link === 'logout') {
+      localStorage.removeItem("token");
+      localStorage.removeItem("activeLink");
+    }
+    const targetLink = link === 'logout' ? 'login' : link === 'dashboard' ? 'admin-dashboard' : `admin/${link}`;
+    navigate(targetLink === 'login' ? '/login' : `/${targetLink}`);
   };
-  const handleLogout = () => {
-    localStorage.clear(); // Clear all items from local storage
-    window.location.href = '/login'; // Redirect to login page
+
+  const checkUsername = async () => {
+    const storedData = localStorage.getItem('access_token');
+    if (storedData) {
+      try {
+        const decodedToken = jwtDecode(storedData);
+        if (decodedToken) {
+          setUserName(decodedToken.name);
+        }
+      } catch (error) {
+        setUserName(null);
+      }
+    } else {
+      setUserName(null);
+    }
   };
 
-  // Define the width for expanded and collapsed sidebar
-  const expandedWidth = "w-15p";
-  const collapsedWidth = "w-5p";
+  useEffect(() => {
+    checkUsername();
+  }, []);
 
   return (
-    <div
-      className={`${
-        isExpanded ? expandedWidth : collapsedWidth
-      } bg-blue-100 h-screen p-5 transition-width duration-300`}
-    >
-      <div className="flex items-center space-x-4 p-2 mb-5">
-        {/* Toggle button */}
-        <button
-          onClick={toggleSidebar}
-          className="text-xl rounded-full p-2 hover:bg-blue-200"
-        >
-          {isExpanded ? <FiX /> : <FiMenu />}
-        </button>
-        {/* Logo and title */}
-        {isExpanded && (
-          <div className="flex-grow">
-            <svg
-              className="h-8 w-8 text-blue-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            ></svg>
-            <span className="text-lg text-blue-800 font-semibold">Staff Dashboard</span>
+    <nav className="h-screen bg-white text-black w-50 flex flex-col justify-between items-center">
+      <div>
+        <div className='mt-8 mb-[55px] flex'>
+          <img src={Profile} alt="Profile" className={`w-[72px] h-[72px]`} />
+          <div className='pl-5 flex items-center'>
+            <div>
+              <h1 className='font-semibold'>Hi,</h1>
+              <h1 className=" font-bold text-xl">{userName}</h1>
+            </div>
+
           </div>
-        )}
+
+        </div>
+        <div className="flex flex-col  items-center">
+
+          <div className={`border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "dashboard" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('dashboard')} className='flex items-center w-full'>
+              <div className='p-3'><DashboardIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>Dashboard</h1>
+            </button>
+          </div>
+
+          <div className={`mt-6 border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "user" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('user')} className='flex items-center w-full'>
+            <div className='p-3'><PersonIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>User Management</h1>
+            </button>
+          </div>
+
+          <div className={`mt-6 border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "tourpackage" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('tourpackage')} className='flex items-center w-full'>
+            <div className='p-3'><ModeOfTravelIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>Tour Package</h1>
+            </button>
+          </div>
+
+          <div className={`mt-6 border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "hotel" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('hotel')} className='flex items-center w-full'>
+            <div className='p-3'><HomeWorkIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>Hotels</h1>
+            </button>
+          </div>
+
+          <div className={`mt-6 border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "payment" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('payment')} className='flex items-center w-full'>
+            <div className='p-3'><PaidIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>Payment</h1>
+            </button>
+          </div>
+
+          <div className={`mt-6 border border-[#DADADA] w-[240px] h-[60px] flex items-center ${activeLink === "feedback" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#FFEEEF]'} rounded-lg`}>
+            <button onClick={handleLinkClick('feedback')} className='flex items-center w-full'>
+            <div className='p-3'><RateReviewIcon /></div>
+              <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px]'>Feedback</h1>
+            </button>
+          </div>
+        </div>
       </div>
-
-      <nav>
-        <a
-          href="/staff-dashboard"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            
-          <AiOutlineDashboard size="20" /></svg>
-          {!isExpanded ? null : <span class="ml-3 text-black">Dashboard</span>}
-
-        </a>
-
-        <a
-          href="/admin/user"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          > <MdTour size ="20"/>  </svg>
-
-          {!isExpanded ? null : (
-            <>
-              <span class="flex-1 ml-3 whitespace-nowrap text-black">Manage Tours</span>
-             
-            </>
-          )}
-          {/* <span class="">Orders</span> */}
-        </a>
-        {/* <a
-          href="/admin/tourpackage"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          ><MdTour size="20" className="mx-auto my-auto"/> </svg>
-          {!isExpanded ? null : <span class="ml-3 text-black">Tour Package</span>}
-          
-        </a> */}
-
-        <a
-          href="#"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          > <IoMdChatboxes size = "20"/> </svg>
-          {!isExpanded ? null : <span class="ml-3 text-black">Chat</span>}
-         
-        </a>
-
-        <a
-          href="#"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          > <MdOutlinePayment size = "20"/> </svg>
-          {!isExpanded ? null : <span class="ml-3 text-black">Payment</span>}
-          {/* <span class="ml-3">Inventory</span> */}
-        </a>
-
-        {/* <a
-          href="/admin/feedback"
-          class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-        >
-          <svg
-            class="w-6 h-6 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          > <MdFeedback size = "20"/> </svg>
-          {!isExpanded ? null : <span class="ml-3 text-black">Feedback</span>}
-         
-        </a> */}
-      </nav>
-      <div className="mt-auto">
-        <a
-          href="#"
-          className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-blue-200"
-          onClick={handleLogout} // Attach the handleLogout function to the onClick event
-        >
-          <MdLogout size="24" color="red" />
-          {!isExpanded ? null : <span className="ml-3 text-black">Logout</span>}
-        </a>
-    </div>
-    </div>
+      <div className={`mt-6 mb-10 bg-[#820008] w-[240px] h-[60px] flex items-center ${activeLink === "logout" ? 'bg-[#FFEEEF] border-transparent' : 'hover:bg-[#820009e6]'} rounded-lg`}>
+        <button onClick={handleLinkClick('logout')} className='flex items-center w-full'>
+          <img src={Logout} alt="Logout" className="block p-3 w-14 h-14" />
+          <h1 className='flex-1 text-center overflow-hidden whitespace-nowrap -ml-4 text-[18px] text-white'>Logout</h1>
+        </button>
+      </div>
+    </nav>
   );
 };
 
 export default StaffSideBar;
+
